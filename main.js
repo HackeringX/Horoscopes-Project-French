0d19b5dd26914aacaa907e8911b09e4732fa5383
нα¢кєяχ <67502949+HackeringX@users.noreply.github.com>
2023-12-13 21:55:26 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,75 @@
+import * as THREE from 'three';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
+
+import "./style.css";
+
+const fov = 60;
+const aspect = window.innerWidth / window.innerHeight;
+const near = 0.1;
+const far = 1000;
+
+// Scene
+const scene = new THREE.Scene();
+
+// Create orb
+const geometryS = new THREE.SphereGeometry(1, 64, 64);
+const materialS = new THREE.MeshBasicMaterial({
+  color: "#FDB813",
+  transparent: true,
+  opacity: 1,
+  
+})
+const mesh = new THREE.Mesh(geometryS, materialS);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+// Light
+const ambientlight = new THREE.AmbientLight(0xffffff, 0.1);
+scene.add(ambientlight);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 8;
+camera.position.x = 0;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias:true, });
+//renderer.autoClear() = false;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
+renderer.render(scene, camera);
+
+// Cool lighting w/ bloomRenderer
+const renderScene = new RenderPass(scene, camera);
+const bloomPass = new UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight),
+  1.5,
+  0.4,
+  0.85
+);
+bloomPass.threshold = 0;
+bloomPass.strength = 2; //intensity of glow
+bloomPass.radius = 0;
+const bloomComposer = new EffectComposer(renderer);
+bloomComposer.setSize(window.innerWidth, window.innerHeight);
+bloomComposer.renderToScreen = true;
+bloomComposer.addPass(renderScene);
+bloomComposer.addPass(bloomPass);
+
+
+const loop = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+  bloomComposer.render();
+}
+
+loop();
