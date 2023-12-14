6d61e5fed83e357fce0b14b7dc0744b4fced9d46
нα¢кєяχ <67502949+HackeringX@users.noreply.github.com>
2023-12-13 22:05:53 -0600

    js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from "./node_modules/three/build/three.module";
+import * as THREE from "./node_modules/three/build/three.module.js";
 import { EffectComposer } from "./node_modules/three/examples/jsm/postprocessing/EffectComposer.js";
 import { RenderPass } from "./node_modules/three/examples/jsm/postprocessing/RenderPass.js";
 import { UnrealBloomPass } from "./node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js";
